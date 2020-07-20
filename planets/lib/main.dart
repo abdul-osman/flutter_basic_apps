90fb3950b292b8c929dd8ddc39bfe5a26357af58
Abdulkadir Osman <abdul.osman87@gmail.com>
2020-07-20 20:18:34 +0100

    added Planets app, an app that shows common information about planets in our solar system

---
+++ planets/lib/main.dart
@@ -0,0 +1,231 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:planets/details_page.dart';
+import 'constants.dart';
+import 'data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planets',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [gradientStartColor, gradientEndColor],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.3, 0.7],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Explore',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 44,
+                        color: const Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    DropdownButton(
+                      items: [
+                        DropdownMenuItem(
+                          child: Text(
+                            'Solar System',
+                            style: TextStyle(
+                              fontFamily: 'Avenir',
+                              fontSize: 24,
+                              color: const Color(0x7cdbf1ff),
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                      onChanged: (value) {},
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Image.asset('assets/drop_down_icon.png'),
+                      ),
+                      underline: SizedBox(
+                        height: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 500,
+                padding: const EdgeInsets.only(left: 32.0),
+                child: Swiper(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: planets.length,
+                  itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                  layout: SwiperLayout.STACK,
+                  pagination: SwiperPagination(
+                    builder: DotSwiperPaginationBuilder(
+                      activeSize: 20,
+                      space: 8,
+                    ),
+                  ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                                pageBuilder: (context, a, b) => DetailsPage(
+                                      planetInfo: planets[index],
+                                    )));
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              SizedBox(height: 100),
+                              Card(
+                                elevation: 8,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32),
+                                ),
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(32.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      SizedBox(height: 100),
+                                      Text(
+                                        planets[index].name,
+                                        style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 44,
+                                          color: const Color(0xff47455f),
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Text(
+                                        'Solar System',
+                                        style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 23,
+                                          color: primaryTextColor,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      SizedBox(height: 32),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Know more',
+                                            style: TextStyle(
+                                              fontFamily: 'Avenir',
+                                              fontSize: 18,
+                                              color: secondaryTextColor,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                          Icon(
+                                            Icons.arrow_forward,
+                                            color: secondaryTextColor,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Hero(
+                            tag: planets[index].position,
+                            child: Image.asset(planets[index].iconImage),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(36.0),
+          ),
+          color: navigationColor,
+        ),
+        padding: const EdgeInsets.all(24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(
+              Icons.menu,
+              color: Colors.white,
+              size: 40.0,
+            ),
+            Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 40.0,
+            ),
+            Icon(
+              Icons.account_box,
+              color: Colors.white,
+              size: 40.0,
+            ),
+            // IconButton(
+            //     icon: Image.asset('assets/menu.png'),
+            //     onPressed: () {},
+            //     ),
+            // IconButton(
+            //     icon: Image.asset('assets/search.png'), onPressed: () {}),
+            // IconButton(
+            //     icon: Image.asset('assets/user.png'), onPressed: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
